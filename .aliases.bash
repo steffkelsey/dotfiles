b9da68f50ba00fd803179645dd5116993154626b
steffkelsey <thecodeviking@gmail.com>
2025-03-23 23:21:28 -0400

    Fixes autocompletion with g alias

--- .aliases.bash
+++ .aliases.bash
@@ -1,14 +1,20 @@
-# Unix
-alias g="git"
+# file 
 alias lal="ls -al"
 alias mkdir="mkdir -p"
-alias e="$EDITOR"
-alias v="$VISUAL"
-alias vim=nvim
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
+
+# editors/vim/neovim
+alias e="$EDITOR"
+alias v="$VISUAL"
+alias vim=nvim
+
+# The git exec
+alias g="git"
+source /usr/share/bash-completion/completions/git
+__git_complete g __git_main
 
 # thefuck
 #eval "$(thefuck --alias)"
